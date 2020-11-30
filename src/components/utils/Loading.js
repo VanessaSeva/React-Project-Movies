e1a8c6e0000a7c4c546f2ebcacd5a8a00071b4e9
Vanessa Seva <vanessa.seva@fabrick.com>
2020-11-30 16:15:06 +0100

    ajout gif

---
+++ src/components/utils/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default () => {
+    return (
+        <div style={{ minHeight: '100vh'}} className="d-flex flex-row justify-content-center align-items-center w-100">
+            <img alt="loading gif" src="https://media1.tenor.com/images/3a0c9909b542717ce9f651d07c4d4592/tenor.gif?itemid=8985245"/>
+        </div>
+
+    )
+}
